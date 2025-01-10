82285ba2623c9223aa49d90f2b8ea77301305c34
kevin <kevin@KEVINANDEL>
2025-01-10 11:03:14 +0530

    Add project files.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+        }
+    }
+}
